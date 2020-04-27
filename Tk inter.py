7f8dbc5cc6cafa23a1de88dc521a153468716079
Pingvin123321 <63117799+Pingvin123321@users.noreply.github.com>
2020-04-27 19:08:51 +0200

    Add files via upload

--- Tk inter.py
+++ Tk inter.py
@@ -1,10 +1,15 @@
-import tkinter as tk
+from tkinter import *
 
+root = Tk()
+a=[]
+e = Entry(root, width=50, bg= "black",fg= "green", borderwidth="5")
+e.pack()
+e.insert(0,"Ime kompanije:")
+def klik ():
+    tekst= e.get()
+    label= Label(root, text=tekst )
+    label.pack()
+gumb= Button(root,text="Click for graph", padx=25, pady= 25,command=klik, fg= 'blue', bg='green')
+gumb.pack()
 
-button = tk.Button(
-    text="Klikni me",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow",
-)
+root.mainloop()
