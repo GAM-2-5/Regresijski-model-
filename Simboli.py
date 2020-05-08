3dd7b220bd1703e71dcf0b646571783a3493581d
Pingvin123321 <63117799+Pingvin123321@users.noreply.github.com>
2020-05-08 23:54:26 +0200

    Add files via upload

--- Simboli.py
+++ Simboli.py
@@ -1,16 +1,18 @@
 import requests
 
 
+
 def get_symbol(symbol):
     url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
 
     result = requests.get(url).json()
 
     for x in result['ResultSet']['Result']:
-        if x['name'] == symbol:
-            return x['symbol']
+        if x['symbol'] == symbol:
+            return x['name']
 
 
-simblo = get_symbol("Microsoft Corporation")
+company = get_symbol("Microsoft Corporation")
 
 print(company)
+
--- Tk inter.py
+++ Tk inter.py
@@ -1,15 +1,76 @@
 from tkinter import *
+import requests
 
 root = Tk()
-a=[]
-e = Entry(root, width=50, bg= "black",fg= "green", borderwidth="5")
-e.pack()
-e.insert(0,"Ime kompanije:")
+root.title("Prediktor cijene dionica")
+label1=Label(root, text= "Puno ime kompanije :",borderwidth=5, relief="groove",fg= 'deep sky blue')
+
+e = Entry(root, width=35,borderwidth=5, bg= "white",fg= "blue",)
+
+e.grid(row=0, column=1, padx=10, pady=10)
+
+
+
+
+a= ""                                                
 def klik ():
-    tekst= e.get()
-    label= Label(root, text=tekst )
-    label.pack()
-gumb= Button(root,text="Click for graph", padx=25, pady= 25,command=klik, fg= 'blue', bg='green')
-gumb.pack()
+    global a 
+    a= e.get()
+
+   
+gumb= Button(root,text="Click for graph", padx=100, pady= 25,command=klik, fg= 'blue', bg='sky blue')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__klik__":
+    klik()
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+
+    result = requests.get(url).json()
+
+    for x in result['ResultSet']['Result']:
+        if x['name'] == symbol:
+            return x['symbol']
+label1.grid(row=0,column=0)
+
+gumb.grid(row=2,sticky=N+S+E+W,columnspan=2)
+
 
 root.mainloop()
+
+e=get_symbol(a)
+
+
